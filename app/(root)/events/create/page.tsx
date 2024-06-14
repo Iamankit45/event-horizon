01cd5b3eb75e3b79a07b519946abc926c108be97
ANKIT <kumarankit0327@gmail.com>
2024-06-14 11:30:53 +0530

    create event page completed

--- app/(root)/events/create/page.tsx
+++ app/(root)/events/create/page.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import EventForm from "@/components/shared/EventForm"
+import { auth } from "@clerk/nextjs";
 
-const createEvent = () => {
+const CreateEvent = () => {
+  const { sessionClaims } = auth();
+
+  const userId = sessionClaims?.userId as string;
+
   return (
-    <div>createEvent</div>
+    <>
+      <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+        <h3 className="wrapper h3-bold text-center sm:text-left">Create Event</h3>
+      </section>
+
+      <div className="wrapper my-8">
+        <EventForm userId={userId} type="Create" />
+      </div>
+    </>
   )
 }
 
-export default createEvent
+export default CreateEvent
---
+++ lib/actions/event.actions.ts
@@ -0,0 +1,41 @@
+'use server'
+
+import { revalidatePath } from 'next/cache'
+
+import { connectToDatabase } from '@/lib/database'
+import Event from '@/lib/database/models/event.model'
+import User from '@/lib/database/models/user.model'
+import Category from '@/lib/database/models/category.model'
+import { handleError } from '@/lib/utils'
+
+import {
+  CreateEventParams,
+  
+} from '@/types'
+
+const getCategoryByName = async (name: string) => {
+  return Category.findOne({ name: { $regex: name, $options: 'i' } })
+}
+
+const populateEvent = (query: any) => {
+  return query
+    .populate({ path: 'organizer', model: User, select: '_id firstName lastName' })
+    .populate({ path: 'category', model: Category, select: '_id name' })
+}
+
+// CREATE
+export async function createEvent({ userId, event, path }: CreateEventParams) {
+  try {
+    await connectToDatabase()
+
+    const organizer = await User.findById(userId)
+    if (!organizer) throw new Error('Organizer not found')
+
+    const newEvent = await Event.create({ ...event, category: event.categoryId, organizer: userId })
+    revalidatePath(path)
+
+    return JSON.parse(JSON.stringify(newEvent))
+  } catch (error) {
+    handleError(error)
+  }
+}
